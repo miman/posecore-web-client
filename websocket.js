367607b40fad3235f95ef1b2548097eb06be6136
Mikael Thorman <mikael.thorman@gmail.com>
2018-08-18 17:50:16 +0200

    initial version

---
+++ websocket.js
@@ -0,0 +1,56 @@
+/**
+ * This is a websocket class that takes messages from the web-socket & dispatches them according to redux actions.
+ * There is also a send function to send msgs on the websocket.
+ */
+class WebSocketRedux {
+  /**
+   * Constructor
+   * @param {The dispatch function for redux} dispatch
+   */
+  constructor() {
+    this.socket = null;
+  };
+
+  /**
+   * Initializes the socket
+   * @param {The websocket URL} url
+   */
+  initializeSocket(url) {
+    this.socket = new WebSocket(url)
+
+    // Initialize socket handlers
+    this.socket.onopen = this.onSocketOpen.bind(this);
+    this.socket.onmessage = this.onMessage.bind(this);
+  };
+
+  /**
+   * Sends the given data JSON:ified on the websocket
+   * @param {the messge to send} msg
+   */
+  sendMsg(msg) {
+    this.socket.send(JSON.stringify(msg))
+  }
+
+  /**
+   * called when the websocket connection is established
+   */
+  onSocketOpen() {
+    /*        this.socket.send(JSON.stringify({
+                type: 'PLAYER_ADDED',
+                name: 'Picard'
+              }))*/
+  }
+
+  /**
+   * This function is called whenever we receive a message on the websocket.
+   * @param {The received event} event
+   */
+  onMessage(event) {
+    console.log('Websocket event received: ' + event.data);
+    const data = JSON.parse(event.data)
+    console.log('received type: ' + data.type);
+    //        this.dispatch(data);
+  }
+}
+
+export default WebSocketRedux;
